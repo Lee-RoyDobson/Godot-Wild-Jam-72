5c71cef0c82e4e21bbeb68839a3b7362cc742801
David Mulgrew <bigmonmulgrew@users.noreply.github.com>
2024-08-10 14:37:12 +0100

    Added a finish objective

---
+++ FinishLevel.gd
@@ -0,0 +1,20 @@
+extends Node3D
+
+@export var next_level = "res://debug_level.tscn"
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	print(body.name)
+	if body.name != "Player":
+		print("Not a player, ignoring")
+		return
+	print("Flag reached")
+	get_tree().change_scene_to_file(next_level)
