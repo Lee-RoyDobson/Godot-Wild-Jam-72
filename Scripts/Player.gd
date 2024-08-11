8cbee16fd44b03c8dfd777bf25ab0eabcff163c3
Stondonk <jastical66@gmail.com>
2024-08-11 13:22:33 +0100

    Improved Player Movement stuff

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,12 +1,19 @@
 extends CharacterBody3D
 class_name Player
 
-@export var SPEED = 5.0
+@export var Speed = 5.0
+@export var Air_Speed = 7.5
 @export var JUMP_VELOCITY = 4.5
+
+@export var Acceleration = 30.0
+@export var Decceleration = 40.0
+@export var AirAcceleration = 15.0
+@export var AirDrag = 3.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 var mouse_motion := Vector2.ZERO
+var mouse_sens = 1
 
 @onready var camera_pivot: Node3D = $CameraPivot
 
@@ -21,19 +28,24 @@ func _physics_process(delta):
 		velocity.y -= gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("action_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+	
+	var onfloor = is_on_floor()
+	var acc_val = Acceleration 
+	if !is_on_floor(): acc_val = AirAcceleration
+	if !direction:
+		acc_val = Decceleration
+		if !is_on_floor(): acc_val = AirDrag
+	
+	var flat_vel:Vector2 = Vector2(get_real_velocity().x,get_real_velocity().z)
+	var next_flat_vel = flat_vel.move_toward(Vector2(direction.x * Speed,direction.z * Speed),acc_val*delta)
+	velocity = Vector3(next_flat_vel.x, velocity.y, next_flat_vel.y)
 
 	move_and_slide()
 
@@ -43,9 +55,7 @@ func _input(event: InputEvent) -> void:
 			mouse_motion = -event.relative * 0.001
 
 func handle_camera_rotation() -> void:
-	rotate_y(mouse_motion.x)
-	camera_pivot.rotate_x(mouse_motion.y)
-	camera_pivot.rotation_degrees.x = clampf(
-		camera_pivot.rotation_degrees.x, -90.0, 90.0
-	)
+	rotate_y(mouse_motion.x*mouse_sens)
+	camera_pivot.rotate_x(mouse_motion.y*mouse_sens)
+	camera_pivot.rotation_degrees.x = clampf(camera_pivot.rotation_degrees.x, -90.0, 90.0)
 	mouse_motion = Vector2.ZERO
--- Scripts/SmoothCamera.gd
+++ Scripts/SmoothCamera.gd
@@ -1,13 +1,17 @@
 extends Camera3D
 
 @export var Camera_speed:= 44.0
+var offset:Vector3
+
+func _ready():
+	offset = position
 
 func _physics_process(delta: float) -> void:
 	var weight = clamp(delta * Camera_speed, 0.0, 1.0)
 	
-	global_transform = global_transform.interpolate_with(
-		get_parent().global_transform, weight
-	)
-	global_position = get_parent().global_position #+ Vector3(0.0, 0.55, -0.238)
-	global_position.y += 0.5
-	global_position.x += 0.5
+	#global_transform = global_transform.interpolate_with(
+		#get_parent().global_transform, weight
+	#)
+	position = offset #+ Vector3(0.0, 0.55, -0.238)
+	#global_position.y += 0.5
+	#global_position.x += 0.5
