9eb2cc14b2c69afd9771e5de790cb6192d692b22
David Mulgrew <bigmonmulgrew@users.noreply.github.com>
2024-08-13 18:21:26 +0100

    clear world objects on level end

--- Scripts/FinishLevel.gd
+++ Scripts/FinishLevel.gd
@@ -12,9 +12,15 @@ func _process(delta: float) -> void:
 	pass
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
+		
 	print(body.name)
 	if body.name != "Duck":
 		print("Not a player, ignoring")
 		return
+		
+	# Clear any object references before moving levels
+	var world_switcher = WorldSwitcher
+	world_switcher.clear_objects()
+	
 	print("Flag reached")
 	get_tree().change_scene_to_file(next_level)
