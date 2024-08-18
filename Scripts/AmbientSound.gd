75ecd25d8fc71f19265f851940186e876c27ceba
Lee-Roy <ltd1999@hotmail.co.uk>
2024-08-18 16:14:01 +0100

    Audio transfers between levels.

--- Scripts/AmbientSound.gd
+++ Scripts/AmbientSound.gd
@@ -3,6 +3,7 @@ extends Node
 var sound_player: AudioStreamPlayer = null
 const LIGHT: AudioStreamMP3 = preload("res://Sounds/Ambient/light.mp3")
 const DARK: AudioStreamMP3 = preload("res://Sounds/Ambient/dark.mp3")
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	sound_player = AudioStreamPlayer.new()
@@ -11,11 +12,15 @@ func _ready() -> void:
 	play_light()
 
 func play_light() -> void:
+	if sound_player.stream == LIGHT:
+		return
 	sound_player.stream = LIGHT
 	sound_player.volume_db = -18
 	sound_player.play(0)
 	
 func play_dark() -> void:
+	if sound_player.stream == DARK:
+		return
 	sound_player.stream = DARK
 	sound_player.volume_db = -18
 	sound_player.play(0)
