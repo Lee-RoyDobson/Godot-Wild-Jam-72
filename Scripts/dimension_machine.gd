b103c49a093b569238cd80d414b6df60cf6809f3
Lee-Roy <ltd1999@hotmail.co.uk>
2024-08-18 16:32:46 +0100

    Readded sfx for dimensions

--- Scripts/dimension_machine.gd
+++ Scripts/dimension_machine.gd
@@ -6,6 +6,6 @@ func _on_area_3d_body_entered(body):
 	# Check if the player entered, if not escape.
 	if not body is Player:
 		return
-
+	body.play_dimension_switch()
 	WorldSwitcher.toggle_world()
 
